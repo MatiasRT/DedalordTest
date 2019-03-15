fe20fa557bd6a69fc1a10d4d9dcb70f660fe0796
Matias <ruiztorresmatias@gmail.com>
2019-03-15 15:51:47 -0300

    Commit #4
    - ScoreController configured

---
+++ DedalordTest/Assets/Scripts/Controllers/ScoreController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreController : MonoBehaviour
+{
+    [SerializeField] int scoreToAdd = 0;
+
+    [SerializeField] TopCollisionController tccScript;
+
+    Text scoreText;
+    private int score;
+
+    void Start()
+    {
+        scoreText = GetComponent<Text>();
+        tccScript.addPoints.AddListener(AddScore);    
+    }
+
+    void AddScore()
+    {
+        score += scoreToAdd;
+        scoreText.text = score + "";
+    }
+}
