4e833ed8562c143a3fc4c8b8951c9bec7977d074
Matias <ruiztorresmatias@gmail.com>
2019-03-15 18:58:42 -0300

    Commit #6
    - Player animation configured

--- DedalordTest/Assets/Scripts/Player/Movement.cs
+++ DedalordTest/Assets/Scripts/Player/Movement.cs
@@ -8,19 +8,30 @@ public class Movement : MonoBehaviour
     [SerializeField] float speed = 0;
     [SerializeField] float startX;
 
-    private Rigidbody2D player;
-
     [SerializeField] TopCollisionController tccScript;
+
+    float animMove;
+
+    private Rigidbody2D player;
+    private Animator anim;
 
     void Start()
     {
         tccScript.restart.AddListener(ReturnToOGPosition);
         player = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
     }
 
     void FixedUpdate()
     { 
         player.velocity = new Vector2(0, Mathf.Lerp(0, Input.GetAxis(input) * speed, 1f));
+    }
+
+    void Update()
+    {
+        animMove = Input.GetAxis(input) * speed;
+
+        anim.SetFloat("move", Mathf.Abs(animMove));
     }
 
     void ReturnToOGPosition()
