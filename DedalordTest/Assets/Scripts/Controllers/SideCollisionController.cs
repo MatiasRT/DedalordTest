5e1defffa1fd242649921523a006d5ad991461b2
Matias <ruiztorresmatias@gmail.com>
2019-03-15 20:35:43 -0300

    Commit #7
    - Enemies configured
    - Side walls configured

---
+++ DedalordTest/Assets/Scripts/Controllers/SideCollisionController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class SideCollisionController : MonoBehaviour
+{
+    [HideInInspector] public UnityEvent restartPos;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        restartPos.Invoke();
+    }
+}
---
+++ DedalordTest/Assets/Scripts/Enemies/EnemyMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] float startX;
+    [SerializeField] float startY;
+
+    [SerializeField] SideCollisionController sccScript;
+
+    private Rigidbody2D enemy;
+
+    void Start()
+    {
+        sccScript.restartPos.AddListener(ReturnToOGPosition);
+        enemy = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        enemy.velocity = new Vector2(Mathf.Lerp(0, speed, 1f) , 0);
+    }
+
+    void ReturnToOGPosition()
+    {
+        enemy.position = new Vector2(startX, startY);
+    }
+}
