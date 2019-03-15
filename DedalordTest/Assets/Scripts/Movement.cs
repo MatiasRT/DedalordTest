15b1fbc92bf12f4fc3e3796dce263bce32903b71
Matias <ruiztorresmatias@gmail.com>
2019-03-14 21:37:29 -0300

    Commit #2
    - gitignore updated
    - sprites added to the project
    - font added to the project
    - player1 and player2 configured

---
+++ DedalordTest/Assets/Scripts/Movement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField] string input;
+    [SerializeField] float speed = 0;
+
+    private Rigidbody2D player;
+
+    void Start()
+    {
+        player = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    { 
+        player.velocity = new Vector2(0, Mathf.Lerp(0, Input.GetAxis(input) * speed, 1f));
+
+        Debug.Log(player.velocity);
+    }
+}
